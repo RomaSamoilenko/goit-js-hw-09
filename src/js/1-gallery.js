c207aeb03c65102e6c76194894c418c7e9cf982e
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-02-09 20:58:34 +0200

    1-gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,23 @@
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+const galleryImg = document.querySelector('.gallery');
 
+const imagesMarkup = galleryItems
+.map(({preview,original,description}) => 
+`<li class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+<img class = "gallery__image"
+  src = "${preview}"
+  data-source = "${original}"
+  alt = "${description}"/>
+</a></li>`).join("");
 
-console.log(SimpleLightbox);
+galleryImg.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
